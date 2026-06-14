bdb07ca6f914de01f920ed55986a52fc04284973
shyamdevelops <shyam.kumar2025@glbajajgroup.org>
2026-06-14 21:59:17 +0530

    Add files via upload

---
+++ day6/Q22.c
@@ -0,0 +1,18 @@
+// C program to convert binary number to decimal number
+
+#include <stdio.h>
+#include <math.h>
+int main() {
+    long long binary;
+    int decimal = 0, i = 0, remainder;
+    printf("Enter a binary number: ");
+    scanf("%lld", &binary);
+    while (binary != 0) {
+        remainder = binary % 10;
+        decimal += remainder * pow(2, i);
+        binary /= 10;
+        i++;
+    }
+    printf("Decimal = %d", decimal);
+    return 0;
+}
