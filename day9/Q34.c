976ff524e2e1817a4d2a6338e4ae7b06416272e8
shyamdevelops <shyam.kumar2025@glbajajgroup.org>
2026-06-17 23:32:20 +0530

    Add files via upload

---
+++ day9/Q34.c
@@ -0,0 +1,13 @@
+/*  program to print reverse number triangle. */
+
+#include <stdio.h>
+int main() {
+    int i, j;
+    for(i = 5; i >= 1; i--) {
+        for(j = 1; j <= i; j++) {
+            printf("%d", j);
+        }
+        printf("\n");
+    }
+    return 0;
+}
