07384989b5d102b2d1a4195b40cad8c9f319394f
shyamdevelops <shyam.kumar2025@glbajajgroup.org>
2026-06-23 23:17:46 +0530

    Add files via upload

---
+++ day15/Q57.c
@@ -0,0 +1,19 @@
+
+// C program to find duplicate elements in an array
+
+
+#include <stdio.h>
+int main() {
+    int arr[100], n, i;
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+    printf("Enter array elements:\n");
+    for(i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    printf("Reversed array: ");
+    for(i = n - 1; i >= 0; i--) {
+        printf("%d ", arr[i]);
+    }
+    return 0;
+}
---
+++ day15/Q59.c
@@ -0,0 +1,23 @@
+// C program to find duplicate elements in an array
+
+
+#include <stdio.h>
+int main() {
+    int arr[100], n, i, last;
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+    printf("Enter array elements:\n");
+    for(i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    last = arr[n - 1];
+    for(i = n - 1; i > 0; i--) {
+        arr[i] = arr[i - 1];
+    }
+    arr[0] = last;
+    printf("Array after right rotation: ");
+    for(i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    return 0;
+}
